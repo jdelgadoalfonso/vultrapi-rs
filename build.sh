3a964cd72bcab494ddbb8dce5ffd531594cdf7b4
Jose Antonio Delgado Alfonso <jose.delgado@galgus.net>
2019-04-15 17:06:11 +0200

    refactor: include openssl in compilation

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Do not forget to run 'cargo clean' before executing this script"
-
-export OPENSSL_INCLUDE_DIR=/usr/include/
-export OPENSSL_LIB_DIR=/usr/lib/ 
-
-OPENSSL_STATIC=yes cargo build $1
-
